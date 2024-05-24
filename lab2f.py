34ffaabd3d9d0aa654e677569cc8c7ab981004d3
Andres Wong <agwong-lam@myseneca.ca>
2024-05-24 00:04:40 -0400

    lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Andres Wong
+# Author_id: agwong-lam
+# 2024/05/23
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
