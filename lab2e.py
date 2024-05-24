f173897f80cb6aea0754afe1ad19f79a30ead3aa
Andres Wong <agwong-lam@myseneca.ca>
2024-05-24 00:01:01 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Andres Wong
+# Author_id: agwong-lam
+# 2024/05/23
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
